e895adf0fd89fb31d4cc14bd19b550dd6f188019
Christopher Pengalilla <christopher.pengalilla@gmail.com>
2024-11-11 19:43:29 +0700

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  daStyle,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+];
+
